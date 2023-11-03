b31df8f24d5c158ceac8d3997390b39d8d56fc4f
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-11-03 02:00:17 -0700

    Generate complete

--- HMM.py
+++ HMM.py
@@ -55,18 +55,56 @@ class HMM:
     ## you do this.
     def generate(self, n):
         """return an n-length observation by randomly sampling from this HMM."""
-        observation = ["#"]
-        for i in range(n-1):
-            last_state = observation[-1]
-            next_states = list(self.transitions[last_state].keys())
-            probabilities = [self.transitions[last_state][state] for state in next_states]
-            # print(next_states)
-            # print(probabilities)
-            next_state = np.random.choice(next_states, p=probabilities)
-            # print(next_state)
-            observation.append(next_state)
+        last_state = "#"
+        states = list(self.transitions[last_state].keys())
+        # print(states)
+        state = random.choices(states, weights=[self.transitions[last_state][s] for s in states])[0]
+        stateseq = [state]
 
-        return observation
+        for _ in range(n - 1):
+            w = [self.transitions[state][s] for s in states]
+            state = random.choices(states, weights=w)[0]
+            stateseq.append(state)
+
+        outputseq = []
+        for i in stateseq:
+            w = [self.emissions[i][j] for j in self.emissions[i]]
+            states = list(self.emissions[i].keys())
+            state = random.choices(states, weights=w)[0]
+            outputseq.append(state)
+
+        return Observation(stateseq, outputseq)
+
+    def forward(self, observation):
+        # possible_states = list(self.transitions.keys())
+        # observation_len = len(observation)
+        #
+        # matrix = np.zeros((len(possible_states), observation_len))
+        # for s in possible_states:
+        #         matrix[possible_states.index(s), 0] = self.transitions[possible_states[0]][s] * self.emissions[s][observation[0]]
+        #
+        # for i in range(1, observation_len):
+        #     for s in possible_states:
+        #         total = 0
+        #         for s2 in possible_states:
+        #             total += matrix[possible_states.index(s2), i - 1] * self.transitions[s2][s] * self.emissions[s][observation[i]]
+        #         matrix[possible_states.index(s), i] = total
+        #
+        # return matrix
+        outputseq = observation.outputseq
+        states = list(self.transitions["#"].keys())
+        total_states = len(states)
+        total_observations = len(observation.stateseq)
+        mat = np.zeros((total_states, total_observations))
+        mat[0][0] = 1.0;
+        for i in range(1, total_observations):
+            for s in states:
+                total = 0
+                for s_prev in states:
+                    total += mat[states.index(s_prev)][i-1] * self.transitions[s_prev][s] * self.emissions[s_prev][outputseq[i]]
+            mat[states.index(s_prev)][i] = total
+
+        return mat
 
     ## you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
     ## determine the most likely sequence of states.
@@ -81,6 +119,9 @@ class HMM:
 
 hmm_obj = HMM()
 hmm_obj.load('two_english')
-print(hmm_obj.transitions)
-print(hmm_obj.emissions)
-print(hmm_obj.generate(20))
+# print(hmm_obj.transitions)
+# print(hmm_obj.emissions)
+observations = hmm_obj.generate(20)
+# print(observations)
+matrix = hmm_obj.forward(observations)
+print(matrix)
