2612c7255b3082aa48b7ee23b51d2dfed21655ae
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-11-03 18:00:17 -0700

    Viterbi algorithm implementation

--- HMM.py
+++ HMM.py
@@ -113,12 +113,53 @@ class HMM:
 
     ## you do this: Implement the Viterbi alborithm. Given an Observation (a list of outputs or emissions)
     ## determine the most likely sequence of states.
-
+    """given an observation,
+            find and return the state sequence that generated
+            the output sequence, using the Viterbi algorithm.
+            """
     def viterbi(self, observation):
-        """given an observation,
-        find and return the state sequence that generated
-        the output sequence, using the Viterbi algorithm.
-        """
+        outputseq = observation.outputseq
+        states = list(self.transitions.keys())
+        total_states = len(states)
+        total_observations = len(observation.outputseq)
+        mat = np.zeros((total_states, total_observations))
+        backpointer = np.zeros((total_states, total_observations))
+        # '#' state should be at index 0
+        for i in range(total_states):
+            if states[i] == '#':
+                states[i] = states[0]
+                states[0] = '#'
+                break
+
+        # Fill out first column
+        for s in states:
+            if s != '#' and outputseq[0] in self.emissions[s]:
+                mat[states.index(s)][0] = self.transitions['#'][s] * self.emissions[s][outputseq[0]]
+
+        # Fill out rest of the columns
+        for i in range(1, total_observations):
+            for s in states:
+                if s != '#':
+                    # total = 0.0
+                    max_prob = 0.0
+                    max_index = 0
+                    for s2 in states:
+                        if s2 != '#' and outputseq[i] in self.emissions[s]:
+                            val = mat[states.index(s2)][i - 1] * self.transitions[s][s2] * self.emissions[s][outputseq[i]]
+                            if max_prob < val:
+                                max_prob = val
+                                max_index = states.index(s2)
+
+                    mat[states.index(s)][i] = max_prob
+                    backpointer[states.index(s)][i] = max_index
+
+        final_output = []
+        max_indices = np.argmax(mat, axis=0)
+        # print(max_indices)
+        # print(mat)
+        for i in max_indices:
+            final_output.append(states[i]);
+        return final_output
 
 
 def read_obs_file(filename):
@@ -154,6 +195,13 @@ if __name__== '__main__':
             observations = read_obs_file(forward)
             for observation in observations:
                 output = hmm_obj.forward(observation)
+                print(output)
+                print(observation.outputseq)
+        if viterbi:
+            hmm_obj.load(filename)
+            observations = read_obs_file(viterbi)
+            for observation in observations:
+                output = hmm_obj.viterbi(observation)
                 print(output)
                 print(observation.outputseq)
 
